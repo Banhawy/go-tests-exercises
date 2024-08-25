900c3ac5e33aa9f568d33118e3f2eb998b69d29e
Adham El Banhawy <the.benhawy@gmail.com>
2024-08-25 21:38:15 +0300

    more test cases, refactor switch block to own func

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris!"
 
 		if got != want {
@@ -13,9 +13,21 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Rickie", "Spanish")
+		want := "Hola, Rickie!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Madamme", "French")
+		want := "Bonjour, Madamme!"
 		assertCorrectMessage(t, got, want)
 	})
 }
