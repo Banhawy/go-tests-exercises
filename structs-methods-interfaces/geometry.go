cade0da6dc643cb6289369a8d1a4dc6b39fdbfb2
Adham El Banhawy <the.benhawy@gmail.com>
2024-08-27 11:30:35 +0300

    refactored to use table driven tests

--- structs-methods-interfaces/geometry.go
+++ structs-methods-interfaces/geometry.go
@@ -2,17 +2,19 @@ package geometry
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
 type Rectangle struct {
 	Height float64
 	Width  float64
 }
+type Circle struct {
+	Radius float64
+}
 
 func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
-}
-
-type Circle struct {
-	Radius float64
 }
 
 func Perimeter(rec Rectangle) float64 {
--- structs-methods-interfaces/geometry_test.go
+++ structs-methods-interfaces/geometry_test.go
@@ -13,25 +13,17 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		want := 100.0
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10.0, 10.0}, 100.0},
+		{Circle{10}, 314.1592653589793},
+	}
 
-		assertCorrectCalculation(got, want, t)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		assertCorrectCalculation(got, want, t)
-	})
-}
-
-func assertCorrectCalculation(got float64, want float64, t *testing.T) {
-	if got != want {
-		t.Errorf("got %g want %g", got, want)
+	for _, tt := range areaTests {
+		if got := tt.shape.Area(); got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
 	}
 }
